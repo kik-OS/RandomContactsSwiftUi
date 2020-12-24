40a0f698551075362b0e80da0f7214e1a4c53253
Nikita Gvozdikov <72789476+kik-OS@users.noreply.github.com>
2020-12-24 20:07:37 +0300

    Initial Commit

---
+++ RandomContacts/RandomContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomContactsApp.swift
+//  RandomContacts
+//
+//  Created by Никита Гвоздиков on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RandomContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
